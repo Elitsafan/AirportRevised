501d16203630dad140d0f706fc71a4bd77847dc1
Elitzafan Ratzon <42668390+Elitsafan@users.noreply.github.com>
2026-01-27 22:34:35 +0200

    update models

---
+++ Airport.Models/SummaryWithMetadata.cs
@@ -0,0 +1,9 @@
+﻿namespace Airport.Models
+{
+    public class SummaryWithMetadata
+    {
+        public required IPagedList<FlightSummary> Summary { get; init; }
+        public int LandingsCount { get; init; }
+        public int DeparturesCount { get; init; }
+    }
+}
