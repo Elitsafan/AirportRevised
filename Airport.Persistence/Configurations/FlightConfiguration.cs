a5c0f9d3e55a13b75c5e7dc36106ad7f0e85f66d
Elitzafan Ratzon <42668390+Elitsafan@users.noreply.github.com>
2026-08-05 16:13:30 +0300

    added sort by entrance index to flight configuration

--- Airport.Persistence/Configurations/FlightConfiguration.cs
+++ Airport.Persistence/Configurations/FlightConfiguration.cs
@@ -2,7 +2,17 @@
 {
     internal class FlightConfiguration
     {
-        public async Task ConfigureAsync(IMongoClient client, IOptions<AirportDbConfiguration> dbConfiguration) =>
-            await Task.CompletedTask;
+        public async Task ConfigureAsync(IMongoClient client, IOptions<AirportDbConfiguration> dbConfiguration)
+        {
+            var collection = client
+                .GetDatabase(dbConfiguration.Value.DatabaseName)
+                .GetCollection<Flight>(dbConfiguration.Value.FlightsCollectionName);
+
+            var indexKeys = Builders<Flight>.IndexKeys.Ascending(f => f.OccupationDetails[0].Entrance);
+
+            var indexModel = new CreateIndexModel<Flight>(indexKeys);
+
+            await collection.Indexes.CreateOneAsync(indexModel);
+        }
     }
 }
