0381d5cbb3dc68d5e14b76488a622c52eee5722d
Elitzafan Ratzon <42668390+Elitsafan@users.noreply.github.com>
2026-08-17 12:14:38 +0300

    fix profile bug, add logging on AuthController, fix local settings

--- Airport.Presentation/Controllers/AuthController.cs
+++ Airport.Presentation/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using Airport.Models.DTOs;
 using Airport.Services.Abstractions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Airport.Presentation.Controllers
 {
@@ -8,14 +9,24 @@ namespace Airport.Presentation.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
+        #region Fields
         private readonly IAuthService _authService;
+        private readonly ILogger<AuthController> _logger;
+        #endregion
 
-        public AuthController(IAuthService authService) => _authService = authService;
+        public AuthController(IAuthService authService, ILogger<AuthController> logger)
+        {
+            _authService = authService;
+            _logger = logger;
+        }
 
         // GET: api/Auth/Login
-        [HttpPost("login")]
+        [HttpPost("[action]")]
         public IActionResult Login([FromBody] LoginCredentials loginDto)
         {
+            if (string.IsNullOrWhiteSpace(loginDto.Username) || string.IsNullOrWhiteSpace(loginDto.Password))
+                _logger.LogWarning("Invalid login credentials.");
+
             var token = _authService.Login(loginDto.Username, loginDto.Password);
 
             if (token is null)
